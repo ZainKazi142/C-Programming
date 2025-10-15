1214f66226e2a3b2435b2b0dca484c6eb31357c3
ZainKazi142 <zainkazi0214@gmail.com>
2025-10-15 22:36:33 +0530

    Add files via upload

---
+++ Exp_2_a.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    /*
+    Name: Zain Mohamed Saeed Kazi
+    UIN: 251P040
+    Roll No: 37
+    Division: C
+    Class: Comps_1
+    */
+
+    int num1;
+    printf("Enter the Number: \n");
+    scanf("%d", &num1);
+    if (num1 % 2 == 0)
+    {
+        printf("It is an Even Number");
+    }
+    else
+    {
+        printf("It is an Odd Number");
+    }
+    return 0;
+}
