66d551c69ca13b71d2fafd117b165a08da373660
ZainKazi142 <zainkazi0214@gmail.com>
2025-10-15 22:09:16 +0530

    Add files via upload

---
+++ Exp_1_a.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    /*
+    Name: Zain Mohamed Saeed Kazi
+    UIN: 251P040
+    Roll No: 37
+    Division: C
+    Class: Comps_1
+    */
+
+    printf("Name: Zain Mohamed Saeed Kazi \n");
+    printf("Age: 18 Years \n");
+    printf("Class: Computer Science 1 \n");
+    printf("UIN: 251P040");
+
+    return 0;
+}
