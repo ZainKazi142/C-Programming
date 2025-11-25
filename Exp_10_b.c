3ae05b98b0f668d4e769cf2c5e9f2ac155329ac6
ZainKazi142 <zainkazi0214@gmail.com>
2025-11-25 22:46:22 +0530

    Add files via upload

---
+++ Exp_10_b.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/* 
+    Name: Zain Mohamed Saeed Kazi
+    Roll No: 37
+    UIN: 251P040
+    Division: C
+*/
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    int *ptr = arr;
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", (ptr + i));  
+    }
+
+    printf("\nArray elements in reverse order:\n");
+    for (int i = n - 1; i >= 0; i--) {
+        printf("%d ", *(ptr + i));   
+    }
+
+    printf("\n");
+    return 0;
+}
